6256ab58cf0c4860ac959bac4582f6d87514a670
n1yatsh1n <116572958+n1yatsh1n@users.noreply.github.com>
2023-06-25 02:48:36 +0600

    Update SquareEquation.cs

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -1,9 +1,37 @@
-﻿namespace SquareEquationLib;
+namespace SquareEquationLib;
 
 public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        throw new NotImplementedException();
+        double eps = 1e-9;
+        
+        if (-eps < a && a < eps || double.IsNaN(a) || double.IsNaN(b) || double.IsNaN(c) || double.IsInfinity(a) || double.IsInfinity(b) || double.IsInfinity(c))
+        {
+            throw new ArgumentException();
+        }
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant <= -eps)
+        {
+            return new double[] { };
+        }
+        else
+        {
+            if (-eps < discriminant && discriminant < eps)
+            {
+                double[] root = new double[1];
+                root[0] = (-b) / 2 * a;
+                return root;
+            }
+            else
+            {
+                double[] roots = new double[2];
+                roots[0] = -(b + Math.Sign(b) * Math.Sqrt(discriminant)) / 2;
+                roots[1] = c / roots[0];
+                return roots;
+            }
+        }
     }
 }
