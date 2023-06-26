37c8600cb612c35b66b7b9bdbd1cb5b34d56bf6b
n1yatsh1n <niyatshinemil43@gmail.com>
2023-06-26 19:27:45 +0000

    UnitTest_Create

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -6,7 +6,7 @@ public class SquareEquation
     {
         double eps = 1e-9;
         
-        if (-eps < a && a < eps || double.IsNaN(a) || double.IsNaN(b) || double.IsNaN(c) || double.IsInfinity(a) || double.IsInfinity(b) || double.IsInfinity(c))
+        if (a < Math.Abs(eps) || double.IsNaN(a) || double.IsNaN(b) || double.IsNaN(c) || double.IsInfinity(a) || double.IsInfinity(b) || double.IsInfinity(c))
         {
             throw new ArgumentException();
         }
@@ -17,21 +17,18 @@ public class SquareEquation
         {
             return new double[] { };
         }
-        else
+        else if (discriminant < Math.Abs(eps))
         {
-            if (-eps < discriminant && discriminant < eps)
-            {
                 double[] root = new double[1];
                 root[0] = (-b) / 2 * a;
                 return root;
-            }
-            else
-            {
+        }
+        else
+        {
                 double[] roots = new double[2];
                 roots[0] = -(b + Math.Sign(b) * Math.Sqrt(discriminant)) / 2;
                 roots[1] = c / roots[0];
                 return roots;
-            }
         }
     }
 }
---
+++ XUnit.Coverlet.MSBuild/UnitTest1.cs
@@ -0,0 +1,60 @@
+using Xunit;
+using System;
+using SquareEquationLib;
+
+namespace SquareEquation.Tests;
+
+public class SquareEquationLib_isUnite
+{
+    [Theory]
+    [InlineData(0, 1, 1)]
+    [InlineData(double.NaN, 1, 1)]
+    [InlineData(1, double.NaN, 1)]
+    [InlineData(1, 1, double.NaN)]
+    [InlineData(double.NegativeInfinity, 1, 1)]
+    [InlineData(1, double.NegativeInfinity, 1)]
+    [InlineData(1, 1, double.NegativeInfinity)]
+    [InlineData(double.PositiveInfinity, 1, 1)]
+    [InlineData(1, double.PositiveInfinity, 1)]
+    [InlineData(1, 1, double.PositiveInfinity)]
+    public void catchingExceptions(double a, double b, double c)
+    {
+        try
+        {
+            SquareEquationLib.SquareEquation.Solve(a, b, c);
+        }
+        catch (Exception exception)
+        {
+            Assert.Equal(exception.GetType(), new ArgumentException().GetType());
+        }
+    }
+
+    [Theory]
+    [InlineData(1, 4, 3, new double[]{-3, -1})]
+    [InlineData(1, -8, 12, new double[]{6, 2})]
+    public void CheckingTwoRoots(double a, double b, double c, double[] roots)
+    {
+        double[] actual = SquareEquationLib.SquareEquation.Solve(a, b, c);
+
+        Assert.Equal(roots, actual);
+    }
+
+    [Theory]
+    [InlineData(1, 2, 1, new double[]{-1})]
+    [InlineData(1, 6, 9, new double[]{-3})]
+    public void CheckingOneRoot(double a, double b, double c, double[] roots)
+    {
+        double[] actual = SquareEquationLib.SquareEquation.Solve(a, b, c);
+
+        Assert.Equal(roots, actual);
+    }
+
+    [Theory]
+    [InlineData(1, 2, 3)]
+    [InlineData(1, 1, 1)]
+    public void CheckinNoRoot(double a, double b, double c)
+    {
+        Assert.True(SquareEquationLib.SquareEquation.Solve(a, b, c).Length == 0);
+    }
+
+}
